17624894b830089e94ef9c2c87a12794c19a0001
OrangeKami <chen.zhang.62@hotamil.com>
2022-07-04 20:16:21 +1000

    logic issue

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,6 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [ :show, :edit, :update, :destroy ]
-  skip_before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!, except: [:index]
   # GET /listings or /listings.json
   def index
     @listings = Listing.all
