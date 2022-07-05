22abc2f2d6890a3187f4d6cb387be30548407064
OrangeKami <chen.zhang.62@hotamil.com>
2022-07-05 23:27:39 +1000

    spelling problem waste 2hours

--- config/application.rb
+++ config/application.rb
@@ -19,8 +19,7 @@ module ChenZhangT2a2App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.eager_load = true
-    config.eager_load_paths += %W(#{config.root}/app/lib)
+   
     
   end
 end
